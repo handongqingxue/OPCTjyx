e06bd9cf121f2b1bc9851056c1df657223f50bf7
handongqingxue <1417140290@qq.com>
2023-02-23 20:05:27 +0800

    完善

--- src/main/java/com/opcTjyx/entity/TaiDaPlc1.java
+++ src/main/java/com/opcTjyx/entity/TaiDaPlc1.java
@@ -3,6 +3,120 @@ package com.opcTjyx.entity;
 public class TaiDaPlc1 {
 
 	private Integer ID;
+	public Integer getID() {
+		return ID;
+	}
+	public void setID(Integer iD) {
+		ID = iD;
+	}
+	public String getD60() {
+		return D60;
+	}
+	public void setD60(String d60) {
+		D60 = d60;
+	}
+	public String getD61() {
+		return D61;
+	}
+	public void setD61(String d61) {
+		D61 = d61;
+	}
+	public String getD62() {
+		return D62;
+	}
+	public void setD62(String d62) {
+		D62 = d62;
+	}
+	public String getD63() {
+		return D63;
+	}
+	public void setD63(String d63) {
+		D63 = d63;
+	}
+	public String getD64() {
+		return D64;
+	}
+	public void setD64(String d64) {
+		D64 = d64;
+	}
+	public String getD65() {
+		return D65;
+	}
+	public void setD65(String d65) {
+		D65 = d65;
+	}
+	public String getD66() {
+		return D66;
+	}
+	public void setD66(String d66) {
+		D66 = d66;
+	}
+	public String getD67() {
+		return D67;
+	}
+	public void setD67(String d67) {
+		D67 = d67;
+	}
+	public String getD68() {
+		return D68;
+	}
+	public void setD68(String d68) {
+		D68 = d68;
+	}
+	public String getD69() {
+		return D69;
+	}
+	public void setD69(String d69) {
+		D69 = d69;
+	}
+	public String getD70() {
+		return D70;
+	}
+	public void setD70(String d70) {
+		D70 = d70;
+	}
+	public String getD71() {
+		return D71;
+	}
+	public void setD71(String d71) {
+		D71 = d71;
+	}
+	public String getD72() {
+		return D72;
+	}
+	public void setD72(String d72) {
+		D72 = d72;
+	}
+	public String getD73() {
+		return D73;
+	}
+	public void setD73(String d73) {
+		D73 = d73;
+	}
+	public String getD74() {
+		return D74;
+	}
+	public void setD74(String d74) {
+		D74 = d74;
+	}
+	public String getD75() {
+		return D75;
+	}
+	public void setD75(String d75) {
+		D75 = d75;
+	}
+	public String getD76() {
+		return D76;
+	}
+	public void setD76(String d76) {
+		D76 = d76;
+	}
+	public Boolean getM584() {
+		return M584;
+	}
+	public void setM584(Boolean m584) {
+		M584 = m584;
+	}
 	private String D60;
 	private String D61;
 	private String D62;
--- src/main/webapp/WEB-INF/view/test.jsp
+++ src/main/webapp/WEB-INF/view/test.jsp
@@ -1,10 +1,30 @@
 <%@ page language="java" contentType="text/html; charset=utf-8"
     pageEncoding="utf-8"%>
+<%
+	String basePath=request.getScheme()+"://"+request.getServerName()+":"
+		+request.getServerPort()+request.getContextPath()+"/";
+%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<script type="text/javascript"
+	src="<%=basePath %>resource/js/jquery-3.3.1.js"></script>
 <title>Insert title here</title>
+<script type="text/javascript">
+var path='<%=basePath %>';
+$(function(){
+	getTaiDaPlc1();
+});
+
+function getTaiDaPlc1(){
+	$.post(path+"variant/getTaiDaPlc1",
+		function(data){
+			
+		}
+	,"json");
+}
+</script>
 </head>
 <body>
 
